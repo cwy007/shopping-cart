066a566fbadad12ba2981005a38fd559c5048acf
chenweiyan <chanweiyan007@gmail.com>
2020-07-15 18:02:11 +0800

    入口文件 src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { Provider } from "react-redux";       // 顶层组件，用于包裹原有的组件
+import { createLogger } from "redux-logger";  // 中间件，日志打印
+import thunk from "redux-thunk";              // 中间件
+import reducer from "./reducers";             // 应用的 Reducer
+import { getAllProducts } from "./actions";   // 应用的 Action
+import App from "./containers/App.jsx";       // 应用的 UI组件
+import "./index.css";                         // 样式
+
+const middleware = [thunk];
+if (process.env.NODE_ENV !== "production") {  // 如果是开发环境，就添加中间件
+  middleware.push(createLogger());
+}
+
+// 创建 Store，整个应用中有且仅有一个 Store，用于存放整个应用中所有的 state
+const store = createStore(reducer, composeWithDevTools(
+  applyMiddleware(...middleware),
+));
+
+// 请求接口获取数据
+store.dispatch(getAllProducts());
+
+render(
+  <Provider store={store}>  {/* 使原来整个应用成为Provider的子组件 */}
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
